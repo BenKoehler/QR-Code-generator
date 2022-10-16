e3701193169c292adafcfad80460e334f15fad33
BenKoehler <ben.koehler@web.de>
2022-10-16 10:44:15 +0200

    replace CMAKE_CURRENT_SOURCE_DIR with PROJECT_SOURCE_DIR

--- bk.cmake
+++ bk.cmake
@@ -1,6 +1,6 @@
-add_library(libQrCodeGenerator STATIC EXCLUDE_FROM_ALL ${CMAKE_CURRENT_SOURCE_DIR}/thirdparty/QR-Code-generator/cpp/qrcodegen.cpp)
+add_library(libQrCodeGenerator STATIC EXCLUDE_FROM_ALL ${PROJECT_SOURCE_DIR}/thirdparty/QR-Code-generator/cpp/qrcodegen.cpp)
 ConfigureCompileOptions(libQrCodeGenerator PRIVATE)
-target_include_directories(libQrCodeGenerator SYSTEM PUBLIC ${CMAKE_CURRENT_SOURCE_DIR}/thirdparty/QR-Code-generator/cpp)
+target_include_directories(libQrCodeGenerator SYSTEM PUBLIC ${PROJECT_SOURCE_DIR}/thirdparty/QR-Code-generator/cpp)
 
 function(link_qr_code_generator target MOD)
     target_link_libraries(${target} ${MOD} libQrCodeGenerator)
